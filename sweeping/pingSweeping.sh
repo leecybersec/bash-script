a75809ef813ef724cd249649c6225063b1087859
Hades <leecybersec@gmail.com>
2021-03-19 04:46:02 -0400

    update upload.php

--- sweeping/pingSweeping.sh
+++ sweeping/pingSweeping.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -z $1 ]; then
+	echo "Usage: $0 <SubIP>"
+	exit
+fi
+
 for ip in $(seq 1 254); do
-   ping -c 1 192.168.107.$ip | grep "bytes from" | cut -d " " -f 4 | cut -d ":" -f 1 &
+   ping -c 1 $1.$ip | grep "bytes from" | cut -d " " -f 4 | cut -d ":" -f 1 &
 done
