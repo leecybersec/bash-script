28870230aab5a6a2231dd71fb7a25f167775beaa
Hades <leecybersec@gmail.com>
2021-04-06 10:44:56 +0700

    update procmon

--- priescal/monitor/procmon.sh
+++ priescal/monitor/procmon.sh
@@ -7,7 +7,7 @@ old_process=$(ps -eo command)
 
 while true; do
 	new_process=$(ps -eo command)
-	diff <(echo "$old_process") <(echo "$new_process") |grep [\<\>] |grep -v "kworker"
+	diff <(echo "$old_process") <(echo "$new_process") |grep [\<\>]
 	sleep 1
 	old_process=$new_process
 done
